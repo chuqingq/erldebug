4b2c9f8e0bcc2e28d3e05753b20a02c7c2e468bf
root <root@cqqdeb>
2013-08-17 06:56:06 -0400

    format syntax

--- d.erl
+++ d.erl
@@ -9,16 +9,15 @@
 c(Module) when is_atom(Module) ->
     case get_module_info(Module) of
         {ok, Source, Options} ->
-            
             %% Module will be loaded dynamicly, except for those are in use.
             case lists:member({d, 'TEST'}, Options) of
                 true ->
                     io:format("debug ON~n");
                 _False -> io:format("debug OFF~n")
-                              end,
+            end,
             c:c(Source, Options);
         Error -> Error
-                     end.
+    end.
 
 d(Module) when is_atom(Module) ->
     case get_module_info(Module) of
@@ -31,10 +30,10 @@ d(Module) when is_atom(Module) ->
                            false ->
                                io:format("debug ON~n"),
                                ?DEBUG_OPTIONS ++ Options
-                                   end,
+                       end,
             c:c(Source, Options1);
         Error -> Error
-                     end.
+    end.
 
 %% Internal functions
 
